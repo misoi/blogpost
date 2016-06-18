44e121eefc40cf2a89654558121d653e93d9bca7
misoi <cytnhia.chepkemoi@gmail.com>
2016-06-18 16:44:22 +0300

    added some code

--- views.py
+++ views.py
@@ -2,10 +2,16 @@ from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from models import Post, Comment
 from flask.ext.mongoengine.wtf import model_form
+from blogpost import app
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return render_template("posts/index.html")
 class ListView(MethodView):
 
     def get(self):
